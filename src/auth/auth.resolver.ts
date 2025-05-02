bdf5470e156d1dc0774608fedd3dbaf24a5d232b
can avci <c.avci@astoundcommerce.com>
2025-05-02 11:31:52 +0300

    MR-0000: service is used to login user

--- src/auth/auth.resolver.ts
+++ src/auth/auth.resolver.ts
@@ -1,13 +1,13 @@
 import { Args, Mutation, Resolver } from '@nestjs/graphql';
-import { UserService } from 'src/user/user.service';
 import { UseGuards } from '@nestjs/common';
 import { Company } from 'src/company_auth/company_auth.decorator';
 import { CompanyAuthGuard } from 'src/company_auth/company_auth.guard';
 import { UserLoginArgs } from './dto/user-login.args';
+import { AuthService } from './auth.service';
 
 @Resolver()
 export class AuthResolver {
-  constructor(private readonly userService: UserService) {}
+  constructor(private readonly authService: AuthService) {}
 
   @UseGuards(CompanyAuthGuard)
   @Mutation(() => String)
@@ -15,7 +15,11 @@ export class AuthResolver {
     @Company() company: any,
     @Args() loginArgs: UserLoginArgs,
   ): Promise<string> {
-    const user = await this.userService.findByUserNameAndPassword(loginArgs);
+    const user = await this.authService.signInByEmailAndPassword({
+      userName: loginArgs.userName,
+      password: loginArgs.password,
+      companyId: company!.id as string,
+    });
 
     return user?.name || 'unknown';
   }
