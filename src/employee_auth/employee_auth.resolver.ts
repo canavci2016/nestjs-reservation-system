8961a575fd7e063a1226cad0ac2e5b99f3199b9f
can avci <c.avci@astoundcommerce.com>
2025-05-15 15:29:45 +0300

    MR-0000:  EmployeeApp_Employee_login service is removed

--- src/employee_auth/employee_auth.resolver.ts
+++ src/employee_auth/employee_auth.resolver.ts
@@ -13,21 +13,6 @@ import { UpdateProfileInput } from './dto/update-profile.input';
 export class EmployeeAuthResolver {
   constructor(private readonly authService: EmployeeAuthService) {}
 
-  @UseGuards(CompanyAppGuard)
-  @Mutation(() => String)
-  async EmployeeApp_Employee_login(
-    @Company() company: any,
-    @Args() loginArgs: EmployeeLoginArgs,
-  ): Promise<string> {
-    const user = await this.authService.signInByEmailAndPassword({
-      userName: loginArgs.userName,
-      password: loginArgs.password,
-      companyId: company!.id as string,
-    });
-
-    return user?.access_token;
-  }
-
   @UseGuards(EmployeeAuthGuard)
   @Query(() => AuthEmployee)
   async EmployeeApp_Employee_profile(
