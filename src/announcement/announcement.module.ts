c06742089ff58b2c7b4bd5983939a9cd8abf945d
can avci <c.avci@astoundcommerce.com>
2025-05-16 14:55:34 +0300

    MR-0000: naming errors in the announcement section is fixed

--- src/announcement/announcement.module.ts
+++ src/announcement/announcement.module.ts
@@ -1,7 +1,7 @@
 import { Module } from '@nestjs/common';
 import { CompanyModule } from 'src/company/company.module';
 import { AnnouncementService } from './announcement.service';
-import { AnnouncementResolver } from './announcementService.resolver';
+import { AnnouncementResolver } from './announcement.resolver';
 
 @Module({
   imports: [CompanyModule],
