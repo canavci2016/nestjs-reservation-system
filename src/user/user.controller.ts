ed8975cc7d00ba57d7a7128b2d8775bee8e38711
can avci <c.avci@astoundcommerce.com>
2025-06-03 16:38:19 +0300

    MR-0000:  token module is implemented to perform relevant operations

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get, Query, UseGuards } from '@nestjs/common';
+import { Controller, Get, UseGuards } from '@nestjs/common';
 import { UserService } from './user.service';
 import { TokenService } from 'src/token/token.service';
 import { TokenGuard } from 'src/token/token.guard';
