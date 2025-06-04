635fffa18d582683dc4cc34f59af4b9150bc9a78
can avci <c.avci@astoundcommerce.com>
2025-06-04 12:37:46 +0300

    MR-0000:  set password approach is adopted

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,14 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import helmet from 'helmet';
+import { join } from 'path';
+import { NestExpressApplication } from '@nestjs/platform-express';
+
 // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment, @typescript-eslint/no-require-imports
 const graphqlUploadExpress = require('graphql-upload/graphqlUploadExpress.js');
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
   app.enableCors({ origin: '*' });
   app.use(
     '/graphql',
@@ -20,6 +23,7 @@ async function bootstrap() {
       crossOriginResourcePolicy: false,
     }),
   );
+  app.useStaticAssets(join(__dirname, '..', 'public'));
 
   await app.listen(process.env.PORT ?? 3000);
 }
--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,8 +1,21 @@
-import { Controller, Get, UseGuards } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Get,
+  NotFoundException,
+  Post,
+  Res,
+  UseGuards,
+  ValidationPipe,
+} from '@nestjs/common';
 import { UserService } from './user.service';
 import { TokenService } from 'src/token/token.service';
 import { TokenGuard } from 'src/token/token.guard';
 import { Token } from 'src/token/token.decorator';
+import { Response } from 'express';
+import Handlebars from 'handlebars';
+import { readFile } from 'fs/promises';
+import { UserSetPasswordInput } from './dto/user-set-password.input';
 
 @Controller('user')
 export class UserController {
@@ -14,9 +27,41 @@ export class UserController {
   @UseGuards(TokenGuard)
   @Get('/set-password')
   async getSetPassword(
-    @Token() tokenModel: { owner_id: string },
-  ): Promise<string> {
+    @Token() tokenModel: { owner_id: string; content: string },
+    @Res() res: Response,
+  ) {
     const user = await this.userService.findOne({ id: tokenModel.owner_id });
-    return 'This action returns all cats';
+
+    const rawContent = await readFile('src/user/views/set-password.hbs', {
+      encoding: 'utf-8',
+    });
+    const template = Handlebars.compile(rawContent);
+    const content = template({
+      fullName: user?.name + ' ' + user?.lastName,
+      token: tokenModel.content,
+    });
+
+    return res.send(content);
+  }
+
+  @UseGuards(TokenGuard)
+  @Post('/set-password')
+  async postSetPassword(
+    @Token()
+    tokenModel: { id: string; owner_id: string; content: string },
+    @Body(new ValidationPipe()) setPasswordDto: UserSetPasswordInput,
+    @Res() res: Response,
+  ) {
+    const user = await this.userService.findOne({ id: tokenModel.owner_id });
+    if (!user) {
+      throw new NotFoundException('user is not found');
+    }
+    const result = await this.userService.updateById(user.id, {
+      password: setPasswordDto.password,
+    });
+    const revokeToken = await this.tokenService.updateById(tokenModel.id, {
+      revoked: true,
+    });
+    return res.send('password is update');
   }
 }
