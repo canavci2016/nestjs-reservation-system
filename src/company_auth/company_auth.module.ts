131620d6256d8d3b192b35a9e1efce94cc5e96ff
can avci <c.avci@astoundcommerce.com>
2025-07-08 12:28:29 +0300

    MR-0000: verbose constants file were erased on company side

--- src/company_auth/company_auth.module.ts
+++ src/company_auth/company_auth.module.ts
@@ -2,13 +2,12 @@ import { Module } from '@nestjs/common';
 import { CompanyAuthService } from './company_auth.service';
 import { CompanyAuthResolver } from './company_auth.resolver';
 import { JwtModule } from '@nestjs/jwt';
-import { jwtConstants } from './constants';
 
 @Module({
   imports: [
     JwtModule.register({
       global: true,
-      secret: jwtConstants.secret,
+      secret: process.env.APP_KEY,
       signOptions: { expiresIn: '10d' },
     }),
   ],
