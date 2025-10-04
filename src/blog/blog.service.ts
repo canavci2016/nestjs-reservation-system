47ab4cd19520f631c444d78c18d820d908dc3917
can avci <c.avci@astoundcommerce.com>
2025-10-04 13:39:45 +0300

    always updated blogs come first

--- src/blog/blog.service.ts
+++ src/blog/blog.service.ts
@@ -36,7 +36,7 @@ export class BlogService {
     const skip = (page - 1) * take;
     query['take'] = take;
     query['skip'] = skip;
-    query['order'] = { createdAt: 'desc' };
+    query['order'] = { updatedAt: 'desc' };
 
     return this.repository.find(query);
   }
