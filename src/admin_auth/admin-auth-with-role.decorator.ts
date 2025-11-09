41dcbdc04d993ce354fa001245b5b8a634a968d3
can avci <c.avci@astoundcommerce.com>
2025-11-09 12:55:04 +0300

    admin auth role is combined with associated enum

--- src/admin_auth/admin-auth-with-role.decorator.ts
+++ src/admin_auth/admin-auth-with-role.decorator.ts
@@ -1,7 +1,8 @@
 import { applyDecorators, SetMetadata, UseGuards } from '@nestjs/common';
 import { AdminAuthGuard } from './admin-auth.guard';
+import { AdminAuthRole } from './admin-auth-role.enum';
 
-export function AdminAuth(...roles: string[]) {
+export function AdminAuth(...roles: AdminAuthRole[]) {
   return applyDecorators(
     SetMetadata('roles', roles),
     UseGuards(AdminAuthGuard),
