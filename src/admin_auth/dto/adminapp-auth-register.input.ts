5018543bf4673f36e01935af18584d6fea2f73ab
can avci <c.avci@astoundcommerce.com>
2025-11-18 15:34:54 +0300

    validation is extended

--- src/admin_auth/dto/adminapp-auth-register.input.ts
+++ src/admin_auth/dto/adminapp-auth-register.input.ts
@@ -1,22 +1,26 @@
 import { Field, InputType } from '@nestjs/graphql';
-import { IsEmail, IsOptional, IsString } from 'class-validator';
+import { IsEmail, IsNotEmpty, IsOptional, IsString } from 'class-validator';
 
 @InputType()
 export class AdminAppAuthRegisterInput {
   @Field()
   @IsString()
+  @IsNotEmpty({ message: 'Şirket ismi boş kalamaz : name' })
   name: string;
 
   @IsOptional()
-  @IsEmail()
+  @IsEmail({}, { message: 'Geçersiz email formatı : email' })
   @Field({ nullable: true })
   email?: string;
 
   @Field()
+  @IsNotEmpty()
   @IsString()
+  @IsNotEmpty({ message: 'Kullanıcı adı boş kalamaz : userName' })
   userName: string;
 
   @Field()
   @IsString()
+  @IsNotEmpty({ message: 'Şifre boş kalamaz : password' })
   password: string;
 }
