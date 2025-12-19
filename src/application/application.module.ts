1bdac9a59771abd6d2b706c1068abd23eccc4e7f
can avci <c.avci@astoundcommerce.com>
2025-12-19 15:27:33 +0300

    Revert "redis connection is setup"
    This reverts commit 57a3355cbf5225028612d76594e48b50c483888c.

--- src/application/application.module.ts
+++ src/application/application.module.ts
@@ -14,9 +14,6 @@ import { AdminAuthModule } from './modules/admin_auth/admin_auth.module';
 import { EmployeeAuthModule } from './modules/employee_auth/employee_auth.module';
 import { EmployeeAvailabilityModule } from './modules/employee_availability/employee-availability.module';
 import { ThrottlerModule } from '@nestjs/throttler';
-import { ThrottlerStorageRedisService } from '@nest-lab/throttler-storage-redis';
-import { ConfigModule } from 'src/core/modules/config/config.module';
-import { ConfigService } from 'src/core/modules/config/config.service';
 
 @Module({
   imports: [
@@ -34,23 +31,12 @@ import { ConfigService } from 'src/core/modules/config/config.service';
     AdminAuthModule,
     EmployeeAuthModule,
     EmployeeAvailabilityModule,
-    ThrottlerModule.forRootAsync({
-      imports: [ConfigModule],
-      inject: [ConfigService],
-      useFactory: (config: ConfigService) => {
-        return {
-          throttlers: [
-            {
-              ttl: 60000,
-              limit: 10,
-            },
-          ],
-          storage: new ThrottlerStorageRedisService(
-            config.get('REDIS_HOST') as string,
-          ),
-        };
+    ThrottlerModule.forRoot([
+      {
+        ttl: 60000, // Time window in milliseconds (1 minute)
+        limit: 10, // Number of requests per time window
       },
-    }),
+    ]),
   ],
   exports: [
     BlogModule,
@@ -69,4 +55,4 @@ import { ConfigService } from 'src/core/modules/config/config.service';
     EmployeeAvailabilityModule,
   ],
 })
-export class ApplicationModule {}
+export class ApplicationModule { }
