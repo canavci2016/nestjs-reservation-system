88096d741a15a8fa3327a74094bc4d14d813b03d
can avci <c.avci@astoundcommerce.com>
2025-12-21 16:37:51 +0300

    announcement service no longer relies on company info in the client side

--- src/application/modules/announcement/client-app.announcement.resolver.ts
+++ src/application/modules/announcement/client-app.announcement.resolver.ts
@@ -3,38 +3,37 @@ import { NotFoundException, UseGuards } from '@nestjs/common';
 import { PaginationInput } from '../../../core/modules/pagination/dto/pagination.input';
 import { AnnouncementService } from './announcement.service';
 import { AnnouncementResponseDto } from './dto/announcement-response.dto';
-import { CompanyAppGuard } from '../company_auth/company_app.guard';
-import { CompanyApp } from '../company_auth/company_app.decorator';
 import { PaginationPipe } from '../../../core/modules/pagination/pagination.pipe';
+import { AuthGuard } from '../auth/auth.guard';
+import { User } from '../auth/auth.decorator';
+import { AuthUserDecoratorInterface } from '../auth/interfaces/auth-employee-decorator.interface';
 
 @Resolver()
 export class ClientAppAnnouncementResolver {
-  constructor(private readonly announcementService: AnnouncementService) {}
+  constructor(private readonly announcementService: AnnouncementService) { }
 
-  @UseGuards(CompanyAppGuard)
+  @UseGuards(AuthGuard)
   @Query(() => [AnnouncementResponseDto])
   async ClientApp_Announcement_list(
-    @CompanyApp()
-    company: { id: string },
+    @User() authUser: AuthUserDecoratorInterface,
     @Args('pagination', { nullable: true }, PaginationPipe)
     pagination: PaginationInput,
   ): Promise<AnnouncementResponseDto[]> {
     const models = await this.announcementService.findAll({
-      companyId: company.id,
+      companyId: authUser.user.companyId,
       pagination: pagination,
     });
     return models;
   }
 
-  @UseGuards(CompanyAppGuard)
+  @UseGuards(AuthGuard)
   @Query(() => AnnouncementResponseDto)
   async ClientApp_Announcement_detail(
-    @CompanyApp()
-    company: { id: string },
+    @User() authUser: AuthUserDecoratorInterface,
     @Args('id') id: string,
   ): Promise<AnnouncementResponseDto> {
     const model = await this.announcementService.findOne({
-      companyId: company.id,
+      companyId: authUser.user.companyId,
       id: id,
     });
 
