180465110092c621d5cc20ae6746876342db953d
can avci <c.avci@astoundcommerce.com>
2025-12-21 16:47:14 +0300

    company profile service no longer relies on company info in the client side

--- src/application/modules/company_auth/company_auth.resolver.ts
+++ src/application/modules/company_auth/company_auth.resolver.ts
@@ -1,16 +1,17 @@
 import { UseGuards } from '@nestjs/common';
 import { Query, Resolver } from '@nestjs/graphql';
-import { CompanyAppGuard } from './company_app.guard';
-import { CompanyApp } from './company_app.decorator';
 import { Company as CompanyModel } from './models/company.model';
 import { CompanyAuthService } from './company_auth.service';
+import { AuthGuard } from '../auth/auth.guard';
+import { AuthUserDecoratorInterface } from '../auth/interfaces/auth-employee-decorator.interface';
+import { User } from '../auth/auth.decorator';
 
 @Resolver()
 export class CompanyAuthResolver {
-  constructor(private readonly authService: CompanyAuthService) {}
-  @UseGuards(CompanyAppGuard)
+  constructor(private readonly authService: CompanyAuthService) { }
+  @UseGuards(AuthGuard)
   @Query(() => CompanyModel)
-  Company_profile(@CompanyApp() company: CompanyModel): CompanyModel {
-    return company;
+  Company_profile(@User() authUser: AuthUserDecoratorInterface) {
+    return this.authService.findById(authUser.user.companyId);
   }
 }
